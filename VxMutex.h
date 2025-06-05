74692df8a9a177d2f3a6563f5141d83a21c27cd3
cy <32183637+jcy1236@users.noreply.github.com>
2025-06-05 13:24:18 +0900

    VxMutex.h 만들기

---
+++ VxMutex.h
@@ -0,0 +1,36 @@
+#include <Windows.h>
+#include <stdlib.h>
+
+class vxHandle
+{
+public:
+    vxHandle();
+
+protected:
+    vxHandle handle_;
+};
+
+class vxMutex : public vxHandle
+{
+public:
+    vxMutex();
+    ~vxMutex();
+    bool Open(const char* mutexName = 0);
+    const char* GetName();
+    bool Lock(DWORD waitTime =INFINITE, DWORD* pdwResult = nullptr);
+    void Unlock();
+    void CLose();
+};
+
+class vxScopeLock : public vxHandle
+{
+public:
+    vxScopeLock(vxMutex* mutex, DWORD waitTime = INFINITE);
+    ~vxScopeLock();
+    void Unlock();
+    void Lock(DWORD waitTime = INIFINITE);
+    bool IsLock();
+    DWORD GetLockResult();
+private:
+    const char* m_comment;
+}
